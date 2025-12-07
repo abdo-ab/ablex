82d78617af875d08c7e2fe114f4322d5997b7abd
Abdo <Qeysabaxa11@gmail.com>
2025-12-07 11:37:31 +0300

    app icon and logo  changed

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,7 +13,7 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, LayoutGrid, PhoneCall } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -26,13 +26,13 @@ const mainNavItems: NavItem[] = [
 
 const footerNavItems: NavItem[] = [
     {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
+        title: 'Contact us',
+        href: 'https://ablex.com/contact',
+        icon: PhoneCall,
     },
     {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
+        title: 'About us',
+        href: 'https://ablex.com/about',
         icon: BookOpen,
     },
 ];
