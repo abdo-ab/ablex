04cafac8cd661000fc4ff5cd1d227550de57c331
Abdo <Qeysabaxa11@gmail.com>
2026-01-02 00:02:52 +0300

    last commit

--- resources/js/pages/PostDetail.tsx
+++ resources/js/pages/PostDetail.tsx
@@ -197,7 +197,7 @@ export default function PostDetails() {
 
             {/* PDF Viewer Modal */}
             <PdfModal
-                fileUrl={route('modules.read', { module: module.id })}
+                fileUrl={module.file_url}
                 title={module.title}
                 isOpen={pdfModalOpen}
                 onClose={() => setPdfModalOpen(false)}
