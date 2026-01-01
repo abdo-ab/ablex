d3ac974169ebbc8e004baca290de3b0177894958
Abdo <Qeysabaxa11@gmail.com>
2026-01-01 22:34:57 +0300

    file upload provider changed

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,18 +59,19 @@ return [
             'throw' => false,
             'report' => false,
         ],
-        'r2' => [
-            'driver' => 's3',
-            'key' => env('R2_ACCESS_KEY_ID'),
-            'secret' => env('R2_SECRET_ACCESS_KEY'),
-            'region' => env('R2_REGION', "us-east-1"),
-            'bucket' => env('R2_BUCKET'),
-            'endpoint' => env('R2_ENDPOINT'),
-            'use_path_style_endpoint' => env('R2_USE_PATH_STYLE_ENDPOINT', false),
-            'url' => env('R2_PUBLIC_URL'),
-            'throw' => false,
-            'visibility' => 'public'
-        ]
+'r2' => [
+    'driver' => 's3',
+    'key' => env('R2_ACCESS_KEY_ID'),
+    'secret' => env('R2_SECRET_ACCESS_KEY'),
+    'region' => 'us-east-1',
+    'bucket' => env('R2_BUCKET'),
+    'endpoint' => env('R2_ENDPOINT'),
+    'url' => env('R2_PUBLIC_URL'),
+    'use_path_style_endpoint' => true,
+    'visibility' => 'public',
+    'throw' => false
+],
+
 
     ],
 
