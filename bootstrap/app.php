3990467baec25f4864e3829d00ea01fb5b0f3bf0
Abdo <Qeysabaxa11@gmail.com>
2026-01-02 14:19:24 +0300

    view error fixed in vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,12 +8,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Throwable;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -23,13 +24,33 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
-
         ]);
-         $middleware->alias([
-                'admin' => ProtectFilamentAdmin::class,
-                'superAdmin' => SuperAdminMiddleware::class,
-            ]);
+
+        $middleware->alias([
+            'admin' => ProtectFilamentAdmin::class,
+            'superAdmin' => SuperAdminMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+
+        
+        //  render Blade error views
+        $exceptions->render(function (Throwable $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        });
+
+    })
+    ->create();
+
+/*
+|--------------------------------------------------------------------------
+| Vercel / Serverless 
+|--------------------------------------------------------------------------
+| Vercel filesystem is read-only.
+| Laravel MUST use /tmp for storage (views, cache, sessions).
+*/
+$app->useStoragePath('/tmp/storage');
+
+return $app;
